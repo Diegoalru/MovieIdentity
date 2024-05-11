39eb6eb05356c2b288a6989362d8f02a48644fb8
Diego Alonso Rubí Salas <diegoalru@gmail.com>
2024-05-11 02:06:48 -0600

    Update Tables Names.

--- MovieIdentity/Areas/Identity/Data/MovieAuth.cs
+++ MovieIdentity/Areas/Identity/Data/MovieAuth.cs
@@ -12,5 +12,14 @@ public class MovieAuth(DbContextOptions<MovieAuth> options) : IdentityDbContext<
         // Customize the ASP.NET Identity model and override the defaults if needed.
         // For example, you can rename the ASP.NET Identity table names and more.
         // Add your customizations after calling base.OnModelCreating(builder);
+        
+        // Rename the table names
+        builder.Entity<MovieUser>().ToTable("Users");
+        builder.Entity<IdentityRole>().ToTable("Roles");
+        builder.Entity<IdentityUserRole<string>>().ToTable("UserRoles");
+        builder.Entity<IdentityUserClaim<string>>().ToTable("UserClaims");
+        builder.Entity<IdentityUserLogin<string>>().ToTable("UserLogins");
+        builder.Entity<IdentityUserToken<string>>().ToTable("UserTokens");
+        builder.Entity<IdentityRoleClaim<string>>().ToTable("RoleClaims");
     }
 }
--- MovieIdentity/Migrations/20240511080505_UpdateTableNames.Designer.cs
+++ MovieIdentity/Migrations/20240511080505_UpdateTableNames.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using MovieIdentity.Areas.Identity.Data;
 
@@ -11,9 +12,11 @@ using MovieIdentity.Areas.Identity.Data;
 namespace MovieIdentity.Migrations
 {
     [DbContext(typeof(MovieAuth))]
-    partial class MovieAuthModelSnapshot : ModelSnapshot
+    [Migration("20240511080505_UpdateTableNames")]
+    partial class UpdateTableNames
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
