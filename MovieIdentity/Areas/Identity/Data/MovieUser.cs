590b319fada67edd0d14c1009804a2e2b0463976
Diego Alonso Rubí Salas <diegoalru@gmail.com>
2024-05-11 02:14:58 -0600

    Update User Model.

--- MovieIdentity/Areas/Identity/Data/MovieUser.cs
+++ MovieIdentity/Areas/Identity/Data/MovieUser.cs
@@ -1,8 +1,15 @@
 ﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
 
 namespace MovieIdentity.Areas.Identity.Data;
 
 public class MovieUser : IdentityUser
 {
-    
+    [Required]
+    [MaxLength(100)]
+    public string FirstName { get; set; } = string.Empty;
+
+    [Required]
+    [MaxLength(100)]
+    public string LastName { get; set; } = string.Empty;
 }
