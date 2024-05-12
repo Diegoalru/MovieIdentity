d5fe532d947636be417f07a4028650610023f4ad
Diego Alonso Rubí Salas <diegoalru@gmail.com>
2024-05-12 03:15:42 -0600

    Add email confirmation status message and admin claim.

--- MovieIdentity/Areas/Identity/Pages/Account/ConfirmEmail.cshtml.cs
+++ MovieIdentity/Areas/Identity/Pages/Account/ConfirmEmail.cshtml.cs
@@ -1,52 +1,51 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
+
 #nullable disable
 
-using System;
-using System.Linq;
+using System.Security.Claims;
 using System.Text;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.WebUtilities;
 using MovieIdentity.Areas.Identity.Data;
 
-namespace MovieIdentity.Areas.Identity.Pages.Account
+namespace MovieIdentity.Areas.Identity.Pages.Account;
+
+public class ConfirmEmailModel(UserManager<MovieUser> userManager, IConfiguration configuration)
+    : PageModel
 {
-    public class ConfirmEmailModel : PageModel
+    /// <summary>
+    ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
+    ///     directly from your code. This API may change or be removed in future releases.
+    /// </summary>
+    [TempData]
+    public string StatusMessage { get; set; }
+
+    /// <summary>
+    ///     Handles the GET request to confirm the user's email.
+    /// </summary>
+    /// <param name="userId">The ID of the user whose email is to be confirmed.</param>
+    /// <param name="code">The confirmation code sent to the user's email.</param>
+    /// <returns>A task that represents the asynchronous operation. The task result contains the IActionResult.</returns>
+    public async Task<IActionResult> OnGetAsync(string userId, string code)
     {
-        private readonly UserManager<MovieUser> _userManager;
+        if (userId == null || code == null) return RedirectToPage("/Index");
 
-        public ConfirmEmailModel(UserManager<MovieUser> userManager)
-        {
-            _userManager = userManager;
-        }
+        var user = await userManager.FindByIdAsync(userId);
+        if (user == null) return NotFound($"Unable to load user with ID '{userId}'.");
 
-        /// <summary>
-        ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
-        ///     directly from your code. This API may change or be removed in future releases.
-        /// </summary>
-        [TempData]
-        public string StatusMessage { get; set; }
-        public async Task<IActionResult> OnGetAsync(string userId, string code)
-        {
-            if (userId == null || code == null)
-            {
-                return RedirectToPage("/Index");
-            }
+        code = Encoding.UTF8.GetString(WebEncoders.Base64UrlDecode(code));
+        var result = await userManager.ConfirmEmailAsync(user, code);
+        StatusMessage = result.Succeeded ? "Thank you for confirming your email." : "Error confirming your email.";
 
-            var user = await _userManager.FindByIdAsync(userId);
-            if (user == null)
-            {
-                return NotFound($"Unable to load user with ID '{userId}'.");
-            }
+        var adminEmail = configuration["AdminEmail"] ?? throw new InvalidOperationException("Admin email not found.");
+        if (!result.Succeeded) return Page();
+        var isAdmin = string.Compare(user.Email, adminEmail, StringComparison.OrdinalIgnoreCase) == 0;
+        await userManager.AddClaimAsync(user,
+            new Claim("IsAdmin", isAdmin.ToString()));
 
-            code = Encoding.UTF8.GetString(WebEncoders.Base64UrlDecode(code));
-            var result = await _userManager.ConfirmEmailAsync(user, code);
-            StatusMessage = result.Succeeded ? "Thank you for confirming your email." : "Error confirming your email.";
-            return Page();
-        }
+        return Page();
     }
 }
